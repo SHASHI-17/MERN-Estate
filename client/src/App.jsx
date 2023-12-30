76342e1760cc014f98326431e2682af21043ae25
SHASHI-17 <hibye31583@gmail.com>
2023-12-30 13:45:46 +0530

    user image using firebase

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,24 @@
-import { BrowserRouter,Route,Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import Signin from "./pages/Signin"
 import Signup from "./pages/SignUp"
 import About from "./pages/About"
-import Profile from "./pages/Profile"
 import Header from "./components/Header"
+import PrivateRoute from "./components/PrivateRoute"
+import Profile from "./pages/Profile"
 
 export default function App() {
   return <BrowserRouter>
-  <Header/>
-      <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/sign-in" element={<Signin/>}/>
-            <Route path="/sign-up" element={<Signup/>}/>
-            <Route path="/about" element={<About/>}/>
-            <Route path="/profile" element={<Profile/>}/>
-      </Routes>
-    </BrowserRouter>
-  
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/sign-in" element={<Signin />} />
+      <Route path="/sign-up" element={<Signup />} />
+      <Route path="/about" element={<About />} />
+      <Route element={<PrivateRoute />}>
+        <Route path="/profile" element={<Profile />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+
 }
