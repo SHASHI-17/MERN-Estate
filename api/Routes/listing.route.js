0bc808a936691ca469a6d31dafca154c31e2ceb8
SHASHI-17 <hibye31583@gmail.com>
2024-01-01 22:23:44 +0530

    search ho gya

--- api/Routes/listing.route.js
+++ api/Routes/listing.route.js
@@ -1,4 +1,4 @@
-const { createListing, deleteListing, updateListing, getListing } = require('../Controllers/listing.controller.js');
+const { createListing, deleteListing, updateListing, getListing, getListings } = require('../Controllers/listing.controller.js');
 const { verifyToken } = require('../utils/verifyUser.js');
 
 const router = require('express').Router();
@@ -7,5 +7,6 @@ router.post('/create',verifyToken,createListing);
 router.delete('/delete/:id',verifyToken,deleteListing);
 router.post('/update/:id',verifyToken,updateListing);
 router.get('/get/:id',getListing);
+router.get('/get',getListings);
 
 module.exports=router
